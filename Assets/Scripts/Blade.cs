7634770d173520643533fbf4136d94bfe6f42332
eatsudev <wizardbolt01@gmail.com>
2024-02-28 12:13:43 +0700

    sgfgdf

--- Assets/Scripts/Blade.cs
+++ Assets/Scripts/Blade.cs
@@ -4,5 +4,49 @@ using UnityEngine;
 
 public class Blade : MonoBehaviour
 {
+    private Camera mainCamera;
+    private Collider bladeCollider;
+    private bool slicing;
     
+    public Vector3 direction { get; private set; }
+    private void Awake()
+    {
+        mainCamera = Camera.main;
+        bladeCollider = GetComponent<Collider>();
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartSlicing();
+        } else if (Input.GetMouseButtonUp(0))
+        {
+            StopSlicing();
+        } else if (slicing)
+        {
+            ContinueSlicing();
+        }
+    }
+    
+    private void StartSlicing()
+    {
+        slicing = true;
+        bladeCollider.enabled = true;
+    }
+
+    private void StopSlicing()
+    {
+        slicing = false;
+        bladeCollider.enabled = false;
+    }
+
+    private void ContinueSlicing()
+    {
+        Vector3 newPosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        newPosition.z = 0f;
+
+        direction = newPosition - transform.position;
+
+        float velocity = direction.magnitude / Time.deltaTime;
+    }
 }
